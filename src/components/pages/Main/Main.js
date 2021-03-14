e3a8686071772c1271ffb4909a46d56e44b694d1
Thyago Rodrigues Silva <trodrsil@everis.com>
2021-03-14 20:36:43 -0300

    Alterações tela principal [Thyago]

--- src/components/pages/Main/Main.js
+++ src/components/pages/Main/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './Main.module.css';
 import img from '../../../assets/hp_book_05.jpg';
 import img2 from '../../../assets/clipart4711592.png';
+import {MdShoppingCart} from 'react-icons/md';
 
 const Main = () => {
   return (
@@ -15,7 +16,7 @@ const Main = () => {
             <a href="localhost:3000/produtos"><h3 className={` ${styles.titleBook} `}><b>Harry Potter and the Order of the Phoenix</b></h3></a>
             <h4>J. K. Rowling</h4>
             <h3 className={` ${styles.priceBook} `}><b>$ 195,60</b></h3>
-            <a href="localhost:3000"><img className={` ${styles.imgAddCart} `} src={img2} /></a>
+            <a href="localhost:3000"> <MdShoppingCart className={` ${styles.imgAddCart} `} size={50} color="#ff0000" /></a>
           </div>
         </div>
 
@@ -25,7 +26,17 @@ const Main = () => {
             <a href="localhost:3000/produtos"><h3 className={` ${styles.titleBook} `}><b>Harry Potter and the Order of the Phoenix</b></h3></a>
             <h4>J. K. Rowling</h4>
             <h3 className={` ${styles.priceBook} `}><b>$ 195,60</b></h3>
-            <a href="localhost:3000"><img className={` ${styles.imgAddCart} `} src={img2} /></a>
+            <a href="localhost:3000"> <MdShoppingCart className={` ${styles.imgAddCart} `} size={50} color="#ff0000" /></a>
+          </div>
+        </div>
+        
+        <div className={` ${styles.card} `}>
+          <a href="localhost:3000/produtos"><img className={` ${styles.image1} `} src={img} /></a>
+          <div className={` ${styles.descriptionCard01} `}>
+            <a href="localhost:3000/produtos"><h3 className={` ${styles.titleBook} `}><b>Harry Potter and the Order of the Phoenix</b></h3></a>
+            <h4>J. K. Rowling</h4>
+            <h3 className={` ${styles.priceBook} `}><b>$ 195,60</b></h3>
+            <a href="localhost:3000"> <MdShoppingCart className={` ${styles.imgAddCart} `} size={50} color="#ff0000" /></a>
           </div>
         </div>
 
@@ -35,7 +46,7 @@ const Main = () => {
             <a href="localhost:3000/produtos"><h3 className={` ${styles.titleBook} `}><b>Harry Potter and the Order of the Phoenix</b></h3></a>
             <h4>J. K. Rowling</h4>
             <h3 className={` ${styles.priceBook} `}><b>$ 195,60</b></h3>
-            <a href="localhost:3000"><img className={` ${styles.imgAddCart} `} src={img2} /></a>
+            <a href="localhost:3000"> <MdShoppingCart className={` ${styles.imgAddCart} `} size={50} color="#ff0000" /></a>
           </div>
         </div>
 
@@ -45,7 +56,7 @@ const Main = () => {
             <a href="localhost:3000/produtos"><h3 className={` ${styles.titleBook} `}><b>Harry Potter and the Order of the Phoenix</b></h3></a>
             <h4>J. K. Rowling</h4>
             <h3 className={` ${styles.priceBook} `}><b>$ 195,60</b></h3>
-            <a href="localhost:3000"><img className={` ${styles.imgAddCart} `} src={img2} /></a>
+            <a href="localhost:3000"> <MdShoppingCart className={` ${styles.imgAddCart} `} size={50} color="#ff0000" /></a>
           </div>
         </div>
 
@@ -55,7 +66,7 @@ const Main = () => {
             <a href="localhost:3000/produtos"><h3 className={` ${styles.titleBook} `}><b>Harry Potter and the Order of the Phoenix</b></h3></a>
             <h4>J. K. Rowling</h4>
             <h3 className={` ${styles.priceBook} `}><b>$ 195,60</b></h3>
-            <a href="localhost:3000"><img className={` ${styles.imgAddCart} `} src={img2} /></a>
+            <a href="localhost:3000"> <MdShoppingCart className={` ${styles.imgAddCart} `} size={50} color="#ff0000" /></a>
           </div>
         </div>
 
@@ -65,7 +76,7 @@ const Main = () => {
             <a href="localhost:3000/produtos"><h3 className={` ${styles.titleBook} `}><b>Harry Potter and the Order of the Phoenix</b></h3></a>
             <h4>J. K. Rowling</h4>
             <h3 className={` ${styles.priceBook} `}><b>$ 195,60</b></h3>
-            <a href="localhost:3000"><img className={` ${styles.imgAddCart} `} src={img2} /></a>
+            <a href="localhost:3000"> <MdShoppingCart className={` ${styles.imgAddCart} `} size={50} color="#ff0000" /></a>
           </div>
         </div>
 
@@ -75,7 +86,207 @@ const Main = () => {
             <a href="localhost:3000/produtos"><h3 className={` ${styles.titleBook} `}><b>Harry Potter and the Order of the Phoenix</b></h3></a>
             <h4>J. K. Rowling</h4>
             <h3 className={` ${styles.priceBook} `}><b>$ 195,60</b></h3>
-            <a href="localhost:3000"><img className={` ${styles.imgAddCart} `} src={img2} /></a>
+            <a href="localhost:3000"> <MdShoppingCart className={` ${styles.imgAddCart} `} size={50} color="#ff0000" /></a>
+          </div>
+        </div>
+
+        <div className={` ${styles.card} `}>
+          <a href="localhost:3000/produtos"><img className={` ${styles.image1} `} src={img} /></a>
+          <div className={` ${styles.descriptionCard01} `}>
+            <a href="localhost:3000/produtos"><h3 className={` ${styles.titleBook} `}><b>Harry Potter and the Order of the Phoenix</b></h3></a>
+            <h4>J. K. Rowling</h4>
+            <h3 className={` ${styles.priceBook} `}><b>$ 195,60</b></h3>
+            <a href="localhost:3000"> <MdShoppingCart className={` ${styles.imgAddCart} `} size={50} color="#ff0000" /></a>
+          </div>
+        </div>
+
+        <div className={` ${styles.card} `}>
+          <a href="localhost:3000/produtos"><img className={` ${styles.image1} `} src={img} /></a>
+          <div className={` ${styles.descriptionCard01} `}>
+            <a href="localhost:3000/produtos"><h3 className={` ${styles.titleBook} `}><b>Harry Potter and the Order of the Phoenix</b></h3></a>
+            <h4>J. K. Rowling</h4>
+            <h3 className={` ${styles.priceBook} `}><b>$ 195,60</b></h3>
+            <a href="localhost:3000"> <MdShoppingCart className={` ${styles.imgAddCart} `} size={50} color="#ff0000" /></a>
+          </div>
+        </div>
+
+        <div className={` ${styles.card} `}>
+          <a href="localhost:3000/produtos"><img className={` ${styles.image1} `} src={img} /></a>
+          <div className={` ${styles.descriptionCard01} `}>
+            <a href="localhost:3000/produtos"><h3 className={` ${styles.titleBook} `}><b>Harry Potter and the Order of the Phoenix</b></h3></a>
+            <h4>J. K. Rowling</h4>
+            <h3 className={` ${styles.priceBook} `}><b>$ 195,60</b></h3>
+            <a href="localhost:3000"> <MdShoppingCart className={` ${styles.imgAddCart} `} size={50} color="#ff0000" /></a>
+          </div>
+        </div>
+
+        <div className={` ${styles.card} `}>
+          <a href="localhost:3000/produtos"><img className={` ${styles.image1} `} src={img} /></a>
+          <div className={` ${styles.descriptionCard01} `}>
+            <a href="localhost:3000/produtos"><h3 className={` ${styles.titleBook} `}><b>Harry Potter and the Order of the Phoenix</b></h3></a>
+            <h4>J. K. Rowling</h4>
+            <h3 className={` ${styles.priceBook} `}><b>$ 195,60</b></h3>
+            <a href="localhost:3000"> <MdShoppingCart className={` ${styles.imgAddCart} `} size={50} color="#ff0000" /></a>
+          </div>
+        </div>
+
+        <div className={` ${styles.card} `}>
+          <a href="localhost:3000/produtos"><img className={` ${styles.image1} `} src={img} /></a>
+          <div className={` ${styles.descriptionCard01} `}>
+            <a href="localhost:3000/produtos"><h3 className={` ${styles.titleBook} `}><b>Harry Potter and the Order of the Phoenix</b></h3></a>
+            <h4>J. K. Rowling</h4>
+            <h3 className={` ${styles.priceBook} `}><b>$ 195,60</b></h3>
+            <a href="localhost:3000"> <MdShoppingCart className={` ${styles.imgAddCart} `} size={50} color="#ff0000" /></a>
+          </div>
+        </div>
+
+        <div className={` ${styles.card} `}>
+          <a href="localhost:3000/produtos"><img className={` ${styles.image1} `} src={img} /></a>
+          <div className={` ${styles.descriptionCard01} `}>
+            <a href="localhost:3000/produtos"><h3 className={` ${styles.titleBook} `}><b>Harry Potter and the Order of the Phoenix</b></h3></a>
+            <h4>J. K. Rowling</h4>
+            <h3 className={` ${styles.priceBook} `}><b>$ 195,60</b></h3>
+            <a href="localhost:3000"> <MdShoppingCart className={` ${styles.imgAddCart} `} size={50} color="#ff0000" /></a>
+          </div>
+        </div>
+
+        <div className={` ${styles.card} `}>
+          <a href="localhost:3000/produtos"><img className={` ${styles.image1} `} src={img} /></a>
+          <div className={` ${styles.descriptionCard01} `}>
+            <a href="localhost:3000/produtos"><h3 className={` ${styles.titleBook} `}><b>Harry Potter and the Order of the Phoenix</b></h3></a>
+            <h4>J. K. Rowling</h4>
+            <h3 className={` ${styles.priceBook} `}><b>$ 195,60</b></h3>
+            <a href="localhost:3000"> <MdShoppingCart className={` ${styles.imgAddCart} `} size={50} color="#ff0000" /></a>
+          </div>
+        </div>
+
+        <div className={` ${styles.card} `}>
+          <a href="localhost:3000/produtos"><img className={` ${styles.image1} `} src={img} /></a>
+          <div className={` ${styles.descriptionCard01} `}>
+            <a href="localhost:3000/produtos"><h3 className={` ${styles.titleBook} `}><b>Harry Potter and the Order of the Phoenix</b></h3></a>
+            <h4>J. K. Rowling</h4>
+            <h3 className={` ${styles.priceBook} `}><b>$ 195,60</b></h3>
+            <a href="localhost:3000"> <MdShoppingCart className={` ${styles.imgAddCart} `} size={50} color="#ff0000" /></a>
+          </div>
+        </div>
+
+        <div className={` ${styles.card} `}>
+          <a href="localhost:3000/produtos"><img className={` ${styles.image1} `} src={img} /></a>
+          <div className={` ${styles.descriptionCard01} `}>
+            <a href="localhost:3000/produtos"><h3 className={` ${styles.titleBook} `}><b>Harry Potter and the Order of the Phoenix</b></h3></a>
+            <h4>J. K. Rowling</h4>
+            <h3 className={` ${styles.priceBook} `}><b>$ 195,60</b></h3>
+            <a href="localhost:3000"> <MdShoppingCart className={` ${styles.imgAddCart} `} size={50} color="#ff0000" /></a>
+          </div>
+        </div>
+
+        <div className={` ${styles.card} `}>
+          <a href="localhost:3000/produtos"><img className={` ${styles.image1} `} src={img} /></a>
+          <div className={` ${styles.descriptionCard01} `}>
+            <a href="localhost:3000/produtos"><h3 className={` ${styles.titleBook} `}><b>Harry Potter and the Order of the Phoenix</b></h3></a>
+            <h4>J. K. Rowling</h4>
+            <h3 className={` ${styles.priceBook} `}><b>$ 195,60</b></h3>
+            <a href="localhost:3000"> <MdShoppingCart className={` ${styles.imgAddCart} `} size={50} color="#ff0000" /></a>
+          </div>
+        </div>
+
+        <div className={` ${styles.card} `}>
+          <a href="localhost:3000/produtos"><img className={` ${styles.image1} `} src={img} /></a>
+          <div className={` ${styles.descriptionCard01} `}>
+            <a href="localhost:3000/produtos"><h3 className={` ${styles.titleBook} `}><b>Harry Potter and the Order of the Phoenix</b></h3></a>
+            <h4>J. K. Rowling</h4>
+            <h3 className={` ${styles.priceBook} `}><b>$ 195,60</b></h3>
+            <a href="localhost:3000"> <MdShoppingCart className={` ${styles.imgAddCart} `} size={50} color="#ff0000" /></a>
+          </div>
+        </div>
+
+        <div className={` ${styles.card} `}>
+          <a href="localhost:3000/produtos"><img className={` ${styles.image1} `} src={img} /></a>
+          <div className={` ${styles.descriptionCard01} `}>
+            <a href="localhost:3000/produtos"><h3 className={` ${styles.titleBook} `}><b>Harry Potter and the Order of the Phoenix</b></h3></a>
+            <h4>J. K. Rowling</h4>
+            <h3 className={` ${styles.priceBook} `}><b>$ 195,60</b></h3>
+            <a href="localhost:3000"> <MdShoppingCart className={` ${styles.imgAddCart} `} size={50} color="#ff0000" /></a>
+          </div>
+        </div>
+
+        <div className={` ${styles.card} `}>
+          <a href="localhost:3000/produtos"><img className={` ${styles.image1} `} src={img} /></a>
+          <div className={` ${styles.descriptionCard01} `}>
+            <a href="localhost:3000/produtos"><h3 className={` ${styles.titleBook} `}><b>Harry Potter and the Order of the Phoenix</b></h3></a>
+            <h4>J. K. Rowling</h4>
+            <h3 className={` ${styles.priceBook} `}><b>$ 195,60</b></h3>
+            <a href="localhost:3000"> <MdShoppingCart className={` ${styles.imgAddCart} `} size={50} color="#ff0000" /></a>
+          </div>
+        </div>
+
+        <div className={` ${styles.card} `}>
+          <a href="localhost:3000/produtos"><img className={` ${styles.image1} `} src={img} /></a>
+          <div className={` ${styles.descriptionCard01} `}>
+            <a href="localhost:3000/produtos"><h3 className={` ${styles.titleBook} `}><b>Harry Potter and the Order of the Phoenix</b></h3></a>
+            <h4>J. K. Rowling</h4>
+            <h3 className={` ${styles.priceBook} `}><b>$ 195,60</b></h3>
+            <a href="localhost:3000"> <MdShoppingCart className={` ${styles.imgAddCart} `} size={50} color="#ff0000" /></a>
+          </div>
+        </div>
+
+        <div className={` ${styles.card} `}>
+          <a href="localhost:3000/produtos"><img className={` ${styles.image1} `} src={img} /></a>
+          <div className={` ${styles.descriptionCard01} `}>
+            <a href="localhost:3000/produtos"><h3 className={` ${styles.titleBook} `}><b>Harry Potter and the Order of the Phoenix</b></h3></a>
+            <h4>J. K. Rowling</h4>
+            <h3 className={` ${styles.priceBook} `}><b>$ 195,60</b></h3>
+            <a href="localhost:3000"> <MdShoppingCart className={` ${styles.imgAddCart} `} size={50} color="#ff0000" /></a>
+          </div>
+        </div>
+
+        <div className={` ${styles.card} `}>
+          <a href="localhost:3000/produtos"><img className={` ${styles.image1} `} src={img} /></a>
+          <div className={` ${styles.descriptionCard01} `}>
+            <a href="localhost:3000/produtos"><h3 className={` ${styles.titleBook} `}><b>Harry Potter and the Order of the Phoenix</b></h3></a>
+            <h4>J. K. Rowling</h4>
+            <h3 className={` ${styles.priceBook} `}><b>$ 195,60</b></h3>
+            <a href="localhost:3000"> <MdShoppingCart className={` ${styles.imgAddCart} `} size={50} color="#ff0000" /></a>
+          </div>
+        </div>
+
+        <div className={` ${styles.card} `}>
+          <a href="localhost:3000/produtos"><img className={` ${styles.image1} `} src={img} /></a>
+          <div className={` ${styles.descriptionCard01} `}>
+            <a href="localhost:3000/produtos"><h3 className={` ${styles.titleBook} `}><b>Harry Potter and the Order of the Phoenix</b></h3></a>
+            <h4>J. K. Rowling</h4>
+            <h3 className={` ${styles.priceBook} `}><b>$ 195,60</b></h3>
+            <a href="localhost:3000"> <MdShoppingCart className={` ${styles.imgAddCart} `} size={50} color="#ff0000" /></a>
+          </div>
+        </div>
+
+        <div className={` ${styles.card} `}>
+          <a href="localhost:3000/produtos"><img className={` ${styles.image1} `} src={img} /></a>
+          <div className={` ${styles.descriptionCard01} `}>
+            <a href="localhost:3000/produtos"><h3 className={` ${styles.titleBook} `}><b>Harry Potter and the Order of the Phoenix</b></h3></a>
+            <h4>J. K. Rowling</h4>
+            <h3 className={` ${styles.priceBook} `}><b>$ 195,60</b></h3>
+            <a href="localhost:3000"> <MdShoppingCart className={` ${styles.imgAddCart} `} size={50} color="#ff0000" /></a>
+          </div>
+        </div>
+
+        <div className={` ${styles.card} `}>
+          <a href="localhost:3000/produtos"><img className={` ${styles.image1} `} src={img} /></a>
+          <div className={` ${styles.descriptionCard01} `}>
+            <a href="localhost:3000/produtos"><h3 className={` ${styles.titleBook} `}><b>Harry Potter and the Order of the Phoenix</b></h3></a>
+            <h4>J. K. Rowling</h4>
+            <h3 className={` ${styles.priceBook} `}><b>$ 195,60</b></h3>
+            <a href="localhost:3000"> <MdShoppingCart className={` ${styles.imgAddCart} `} size={50} color="#ff0000" /></a>
+          </div>
+        </div>
+
+        <div className={` ${styles.card} `}>
+          <a href="localhost:3000/produtos"><img className={` ${styles.image1} `} src={img} /></a>
+          <div className={` ${styles.descriptionCard01} `}>
+            <a href="localhost:3000/produtos"><h3 className={` ${styles.titleBook} `}><b>Harry Potter and the Order of the Phoenix</b></h3></a>
+            <h4>J. K. Rowling</h4>
+            <h3 className={` ${styles.priceBook} `}><b>$ 195,60</b></h3>
+            <a href="localhost:3000"> <MdShoppingCart className={` ${styles.imgAddCart} `} size={50} color="#ff0000" /></a>
           </div>
         </div>
 
