bb70c082141aaae0fbb0e15c25cc3b6b17d09ccb
Felipe <felipediasamorimpessoa@gmail.com>
2021-03-25 08:20:00 -0300

    pagination main page

--- src/components/pages/Main/Main.js
+++ src/components/pages/Main/Main.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import styles from './Main.module.css';
+import stylesPagination from '../Books/Books.module.css';
 import CardBook from './CardBook';
 import { getBooks } from '../../../redux';
 import { GET_BOOKS } from '../../../api';
 import ModalView from '../Books/ModalView';
+import ReactPaginate from 'react-paginate';
 
 const Main = () => {
   const dispatch = useDispatch();
   const { books } = useSelector((state) => state);
   const [showDetailsBook, setShowDetailsBook] = React.useState(null);
+  const [page, setPage] = React.useState(0);
 
   React.useEffect(() => {
     async function takeBooks() {
       try {
-        const { options, url } = GET_BOOKS(0, 6);
+        const { options, url } = GET_BOOKS(page, 10);
 
         const response = await fetch(url, options);
 
@@ -36,7 +39,12 @@ const Main = () => {
     }
 
     takeBooks().then((response) => dispatch(getBooks(response)));
-  }, [dispatch]);
+  }, [dispatch, page]);
+
+  function handlePageClick(e) {
+    setPage(e.selected);
+  }
+
   return (
     <div className={` container ${styles.homeArea}`}>
       {showDetailsBook && (
@@ -55,6 +63,21 @@ const Main = () => {
               setShowDetailsBook={setShowDetailsBook}
             />
           ))}
+      </div>
+      <div className={`${styles.pageCards}`}>
+        <ReactPaginate
+          previousLabel={'Anterior'}
+          nextLabel={'Próximo'}
+          breakLabel={'...'}
+          breakClassName={'break-me'}
+          pageCount={books.totalPages}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={handlePageClick}
+          containerClassName={stylesPagination.pagination}
+          subContainerClassName={'pages pagination'}
+          activeClassName={stylesPagination.active}
+        />
       </div>
     </div>
   );
