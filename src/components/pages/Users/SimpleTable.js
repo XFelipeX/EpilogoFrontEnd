2cd1fb128c71a20b20c5fdbd33efccfcc221322d
Felipe <felipediasamorimpessoa@gmail.com>
2021-04-07 16:08:12 -0300

    config aux components

---
+++ src/components/pages/Users/SimpleTable.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './Users.module.css';
+
+const SimpleTable = ({ accounts }) => {
+  console.log(accounts);
+  return (
+    <table className={`${styles.usersCardTable}`}>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Usuário</th>
+          {/* <th>E-mail</th> */}
+          <th>Tipo</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {accounts &&
+          accounts.content &&
+          accounts.content.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.userName}</td>
+              {/* <td>{user.email}</td> */}
+              <td>{user.typeAccount === 0 ? 'Administrador' : 'Estoquista'}</td>
+              <td>{user.status === 1 ? 'Ativo' : 'Inativo'}</td>
+            </tr>
+          ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default SimpleTable;
