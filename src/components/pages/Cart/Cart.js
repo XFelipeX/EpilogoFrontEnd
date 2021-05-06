99f2a8469c5cfedf53f54024310a51d8122f25d3
Felipe <felipediasamorimpessoa@gmail.com>
2021-05-06 11:58:43 -0300

    view shipping and demand info

--- src/components/pages/Cart/Cart.js
+++ src/components/pages/Cart/Cart.js
@@ -3,12 +3,17 @@ import Header from '../../Header/Header';
 import styles from './Cart.module.css';
 import { useSelector, useDispatch } from 'react-redux';
 import { decrementItem, incrementItem, removeItem } from '../../../redux';
+import { useHistory } from 'react-router-dom';
+// import { calcularPrecoPrazo } from 'correios-brasil';
 
 const Cart = () => {
   const { stateCart } = useSelector((state) => state);
   const [products, setProducts] = React.useState([...stateCart.products]);
+  const history = useHistory();
   const dispatch = useDispatch();
   console.log(stateCart);
+
+  const [cep, setCep] = React.useState('');
 
   React.useEffect(() => {
     const copyArray = stateCart.products;
@@ -31,6 +36,24 @@ const Cart = () => {
   function remove(item) {
     dispatch(removeItem(item));
   }
+
+  function calcShipping(from) {
+    let args = {
+      sCepOrigem: '81200100',
+      sCepDestino: '21770200',
+      nVlPeso: '1',
+      nCdFormato: '1',
+      nVlComprimento: '20',
+      nVlAltura: '20',
+      nVlLargura: '20',
+      nCdServico: ['04014', '04510'],
+      nVlDiametro: '0',
+    };
+    // calcularPrecoPrazo(args).then((response) => {
+    //   console.log(response);
+    // });
+  }
+
   return (
     <div className={styles.container}>
       <Header />
@@ -56,6 +79,7 @@ const Cart = () => {
                     min={1}
                     id={product.id}
                     value={product.quantity}
+                    readOnly
                   />
                   <span className={styles.less} onClick={() => less(product)}>
                     -
@@ -71,7 +95,78 @@ const Cart = () => {
           </ul>
         </article>
         <article className={styles.actionsArea}>
-          <div className={styles.actions}></div>
+          <div className={styles.actions}>
+            <article className={styles.actionsTop}>
+              <div className={styles.shipping}>
+                <label htmlFor="shipping">
+                  <input
+                    type="text"
+                    name="shipping"
+                    id="shipping"
+                    placeholder="CEP"
+                    value={cep}
+                    onChange={({ target }) => setCep(target.value)}
+                  />
+                  <button
+                    type="button"
+                    className={styles.btnCalcShipping}
+                    onClick={() => calcShipping(cep)}
+                  >
+                    Calcular Frete
+                  </button>
+                </label>
+                <span className={styles.shippingOptions}>
+                  <label htmlFor="shippingType1">
+                    <input
+                      type="radio"
+                      name="shippingType"
+                      id="shippingType1"
+                    />
+                    Econômico
+                  </label>
+                  <label htmlFor="shippingType2">
+                    <input
+                      type="radio"
+                      name="shippingType"
+                      id="shippingType2"
+                    />
+                    Sedex
+                  </label>
+                  <label htmlFor="shippingType3">
+                    <input
+                      type="radio"
+                      name="shippingType"
+                      id="shippingType3"
+                    />
+                    PAC
+                  </label>
+                </span>
+              </div>
+              <div className={styles.actionsInfo}>
+                <ul>
+                  <li>Quantidade de itens {stateCart.amount}</li>
+                  <li>
+                    Subtotal <span>R$ {stateCart.subtotal}</span>
+                  </li>
+                  <li>
+                    Total <span>R$ {stateCart.total}</span>
+                  </li>
+                </ul>
+              </div>
+            </article>
+            <article className={styles.actionsBottom}>
+              <button
+                type="button"
+                className={styles.btnBack}
+                onClick={() => history.push('/principal')}
+              >
+                Continuar Comprando
+              </button>
+              <button type="button" className={styles.btnDone}>
+                Finalizar o Pedido
+              </button>
+            </article>
+          </div>
         </article>
       </div>
     </div>
